39e0d4b9521f006306163a322cd56d506f6d64d2
andyv4rga5 <camilo.vargas087@hotmail.com>
2023-12-13 10:05:35 -0500

    fonts

--- App.js
+++ App.js
@@ -1,7 +1,15 @@
 import CategoriesScreen from './src/screens/CategoriesScreen'
+import { useFonts } from 'expo-font'
 
 export default function App() {
+  const [fontLoaded] = useFonts({
+    'Barlow-regular': require('./assets/fonts/Barlow-Regular.ttf'),
+    'Barlow-Bold': require('./assets/fonts/Barlow-Bold.ttf'),
+  })
+
+  console.log(fontLoaded)
+  
   return (
-    <CategoriesScreen/>
+    <CategoriesScreen />
   );
 }
