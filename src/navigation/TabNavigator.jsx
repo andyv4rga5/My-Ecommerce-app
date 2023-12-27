31e464fc14d0ffd4c0f258a7a359df7917888a1f
andyv4rga5 <camilo.vargas087@hotmail.com>
2023-12-27 13:08:11 -0500

    style Bar

--- src/navigation/TabNavigator.jsx
+++ src/navigation/TabNavigator.jsx
@@ -1,3 +1,5 @@
+import {StyleSheet} from "react-native";
+import { colors } from '../global/colors'
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
@@ -10,7 +12,12 @@ const Tab = createBottomTabNavigator()
 const TabNavigator = () => {
     return (
         <NavigationContainer>
-            <Tab.Navigator>
+            <Tab.Navigator
+                screenOption={{
+                    headerShown: false,
+                    tabBarStyle: styles.tabBar,
+                }}
+            >
                 <Tab.Screen name="ShopStack" component={ShopNavigator} />
                 <Tab.Screen name="CartStack" component={CartNavigator} />
             </Tab.Navigator>
@@ -19,3 +26,16 @@ const TabNavigator = () => {
 }
 
 export default TabNavigator
+
+const styles = StyleSheet.create({
+    tabBar:{
+        backgroundColor: colors.primary,
+        shadowColor: colors.shadows,
+        elevation:1,
+        position: "absolute",
+        marginHorizontal: 25,
+        bottom: 25, 
+        height: 60,
+        borderRadius:20,
+    }
+})
