a70105a09859d3bcbbb2fad6b27483d791689dd4
andyv4rga5 <camilo.vargas087@hotmail.com>
2024-01-21 14:12:56 -0500

    previo LogIn sin Clv

--- src/firebase/database.js
+++ src/firebase/database.js
@@ -1,1 +1,3 @@
-export const base_url = 'https://ecommercer-app-default-rtdb.firebaseio.com/'
+// export const base_url = 'https://ecommercer-app-default-rtdb.firebaseio.com/'
+// export const base_auth_url = 'https://identitytoolkit.googleapis.com/v1/'
+// export const key = ''
--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
 import shopReducer from "../features/shopSlice";
 import { setupListeners } from "@reduxjs/toolkit/query";
-import { shopApi } from "../services/shopService";
+import { shopApi } from "../services/shopService"; 
+import { authApi } from "../services/authService";
 import cartReducer from "../features/cartSlice";
+import authReducer from "../features/authSlice";
 
 
 const store = configureStore({
     reducer: {
         shopReducer,
         cartReducer,
-        [shopApi.reducerPath]: shopApi.reducer
+        authReducer,
+        [shopApi.reducerPath]: shopApi.reducer,
+        [authApi.reducerPath]: shopApi.reducer,
     },
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(shopApi.middleware)
+        getDefaultMiddleware().concat(shopApi.middleware).concat(authApi.middleware)
 })
 
 setupListeners(store.dispatch)
