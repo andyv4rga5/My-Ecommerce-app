f0af2345be776b0629b273bf1450d4ee915675d7
andyv4rga5 <camilo.vargas087@hotmail.com>
2024-01-23 13:31:57 -0500

    uri correjida maps

--- src/components/MapPreview.jsx
+++ src/components/MapPreview.jsx
@@ -5,7 +5,7 @@ const maps_api_key = process.env.EXPO_MAPS_API_KEY
 
 const MapPreview = ({ location }) => {
   console.log(location)
-  const mapPreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?zoom=14&size=300x300&maptype=roadmap&markers=color:red%7Clabel:I%7C${location.latitude},${location.longitude}&markers=icon:${icon}%7C${location.latitude + 0.01},${location.longitude}&path=color:0x0000ff%7C${location.latitude},${location.longitude}%7C${location.latitude + 0.01},${location.longitude}&key=${maps_api_key}`
+  const mapPreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?zoom=14&size=300x300&maptype=roadmap&markers=color:red%7Clabel:I%7C${location.latitude},${location.longitude},${location.latitude + 0.01},${location.longitude}&path=color:0x0000ff%7C${location.latitude},${location.longitude}%7C${location.latitude + 0.01},${location.longitude}&key=${maps_api_key}`
   return (
     <View style={styles.mapPreview}>
       <Image
